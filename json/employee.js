9a3eaaf56fbf948ddbab7e6a30956859b42fa7b7
vaallery <34%Repytxtrf>
2018-10-12 13:42:03 +0300

    init requisition

---
+++ json/employee.js
@@ -0,0 +1,11 @@
+export default {
+  // Данные храняться в модели Employee
+  // Описание приведено в качестве примера
+  id: 111,
+  email: "e.muratova@tatar.ru",
+  phone: "8(843)222-09-28",
+  last_name: "Муратова",
+  first_name: "Евгения",
+  middle_name: "Сергеевна",
+  position: "должность"
+}
