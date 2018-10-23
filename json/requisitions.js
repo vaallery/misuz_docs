99d56b13dbe2557fe9da9ed1a334dbeb58a9b8e6
vaallery <34%Repytxtrf>
2018-10-23 16:11:43 +0300

    patch requisition

---
+++ json/requisitions.js
@@ -0,0 +1,22 @@
+export default [
+  //Массив заявок =
+  {
+    id: 123,
+    purchase: {
+      id: '23',
+      object_info: '',
+      placing_way: {
+        name: ''
+      },
+      organization: {
+        inn: '',
+        short_name: ''
+      },
+      max_price: '',
+      updated_at: '',
+    },
+    state: '',
+    available_states: ['edit', 'show', 'send_to_fix']
+  }
+]
+
